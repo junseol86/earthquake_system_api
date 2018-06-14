c1578a67afca62c8f5d7de42fb8dffb66bb8ee06
고현민 <junseol86@gmail.com>
2018-06-15 01:31:01 +0900

    Add static images

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/load_str', rt_load_str);
